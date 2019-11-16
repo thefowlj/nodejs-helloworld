6cfefda4d8e55d331bb908f11cf4858b8ec5ec04
jonfowler <jfowler27@gmail.com>
2019-11-16 16:01:49 -0700

    Initial commit

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+//hello-world.js
+//basic hello world node.js server 
+const http = require('http');
+const port = 3001;
+
+http.createServer(function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, "127.0.0.1");
+console.log(`Server running at http://127.0.0.1:${port}`);
